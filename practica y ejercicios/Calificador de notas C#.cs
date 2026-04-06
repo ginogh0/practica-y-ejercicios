88b7fb5c9694be7f409e94aa4811467f87e6f9d1
Gino <ginojuegos7@gmail.com>
2026-04-06 15:06:04 -0300

    Add files via upload

---
+++ practica y ejercicios/Calificador de notas C#.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("====== Introduce tu nota del examen: ======");
+int nota = Convert.ToInt32(Console.ReadLine());
+if (nota > 6)
+{
+    Console.WriteLine("Has aprobado el examen");
+}
+else if (nota < 6 && nota > 4)
+{
+    Console.WriteLine("Debes repetir el examen");
+}
+else
+{
+    Console.WriteLine("Has reprobado el examen");
+}
+;
+
+Console.ReadKey();
