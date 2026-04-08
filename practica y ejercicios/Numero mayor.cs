276d4981765c679299f867fecd389548ab924bef
Gino <ginojuegos7@gmail.com>
2026-04-08 13:19:26 -0300

    Add files via upload

---
+++ practica y ejercicios/Numero mayor.cs
@@ -0,0 +1,45 @@
+﻿string repetir = "s";
+
+while (repetir == "s")
+{
+    Console.WriteLine("===== Ingrese dos numeros enteros: =====");
+
+    int numero = Convert.ToInt32(Console.ReadLine());
+    int numero2 = Convert.ToInt32(Console.ReadLine());
+
+    if (numero < numero2)
+    {
+        Console.WriteLine("El numero " + numero + " es menor que el numero " + numero2);
+    }
+    else if (numero > numero2)
+    {
+        Console.WriteLine("El numero " + numero + " es mayor que el numero " + numero2);
+    }
+    else
+    {
+        Console.WriteLine("Los numeros son iguales");
+    }
+
+    Console.WriteLine("¿Querés repetir? (s/n)");
+    repetir = Console.ReadLine();
+    if (repetir != "s")
+    {
+        Console.WriteLine("====== Fin del programa ======");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.ReadKey();
